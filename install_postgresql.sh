363d11ada1f4cf4936d3c1de41e44b19f02f643a
AldricVS <aldric.silvestre@outlook.fr>
2022-04-05 17:44:38 +0200

    Create script for postgresql

---
+++ install_postgresql.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+# Go in pds-global repository, "docs/tutos" folder to have infos on postgresql setup
+
+sudo apt-get install postgresql postgresql-contrib postgresql-server-dev-12 -y
